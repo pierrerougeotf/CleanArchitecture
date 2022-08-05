d38a492a77d176a01f77ac3727fbef0cc19abbad
Pierre Rougeot <pierre.rougeot@fabernovel.com>
2022-08-06 01:30:57 +0200

    Initial Commit

---
+++ Sources/CleanArchitecture/CleanArchitecture.swift
@@ -0,0 +1,6 @@
+public struct CleanArchitecture {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CleanArchitectureTests/CleanArchitectureTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CleanArchitecture
+
+final class CleanArchitectureTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CleanArchitecture().text, "Hello, World!")
+    }
+}

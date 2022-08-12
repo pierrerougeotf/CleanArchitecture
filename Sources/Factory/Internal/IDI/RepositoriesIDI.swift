d63b8a80a5755d3269d386e3b028ab658fac436a
Pierre Rougeot <pierre.rougeot@fabernovel.com>
2022-08-12 02:14:18 +0200

    Add RepositoriesIDI

---
+++ Sources/Factory/Internal/IDI/RepositoriesIDI.swift
@@ -0,0 +1,11 @@
+//
+//  RepositoriesIDI.swift
+//  
+//
+//  Created by Pierre Rougeot on 12/08/2022.
+//
+
+class RepositoriesIDI {
+    let stack = Stack<Number>()
+    let computation = Computation<Number, Operation>()
+}
